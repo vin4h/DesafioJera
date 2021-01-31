c26e71a98b9197f7450fff50e48eb6e4599a4d77
Vin4h <viniciu.rezende@gmail.com>
2021-01-31 10:18:24 -0400

    migrations Users & Profile

---
+++ src/database/migrations/1612099662639-CreateUsers.ts
@@ -0,0 +1,57 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreateUsers1612099662639 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'users',
+                columns: [
+                    {
+                        name:'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'facebook_id',
+                        type: 'varchar',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'birthday',
+                        type: 'Date',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
+    }
+
+}
---
+++ src/database/migrations/1612100717981-CreateProfile.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
+
+export class CreateProfile1612100717981 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'profiles',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'user_id',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+                ]
+            })
+        );
+
+        await queryRunner.createForeignKey('profiles', new TableForeignKey({
+            name:'ProfileUser',
+            columnNames: ['user_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'users',
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE'
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey('profiles','ProfileUser');
+
+        await queryRunner.dropTable('profiles');
+    }
+
+}
